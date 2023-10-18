afe6290d3e9c72829886c4587f966a7628ca1f1f
duduzx02 <duduzx02@gmail.com>
2023-10-18 00:34:20 -0300

    Conexão Http estabelecida

--- src/App.java
+++ src/App.java
@@ -1,10 +1,22 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception{
 
         // Fazer uma conexão HTTP e buscar os top 250 filmes
 
         String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+
+        System.out.println(body);
+
 
         // limpar os dados que interressam (titulo, img e rank) passear os dados
         // Exibir e manipular os dados na aplicação
