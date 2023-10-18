b3353a2917ec9f5790f802f819e97e83d3c121ca
duduzx02 <duduzx02@gmail.com>
2023-10-18 00:05:03 -0300

    First commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // Fazer uma conexão HTTP e buscar os top 250 filmes
+
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+
+        // limpar os dados que interressam (titulo, img e rank) passear os dados
+        // Exibir e manipular os dados na aplicação
+
+
+    }
+}
